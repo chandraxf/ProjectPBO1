30934c3342770027a285c6079f46c57a712a19f0
CHDHX <CHDHX@CHDHX-PC>
2021-06-19 09:11:34 +0700

    Pembuatan Halaman utama dengan 8 Atribut, 4 Method, dan 3 Constructor

--- src/pembayaran/halamanutama.java
+++ src/pembayaran/halamanutama.java
@@ -1,34 +1,79 @@
 package pembayaran;
 import java.util.Scanner;
 
-//Construktor
-    class pembayaran{
-    String nama;
-    String npm;
-    String jurusan;
+    //Construktor
+    class pubg{
     
-    pembayaran(){
-        System.out.println("Test Kontuktur");
     }
+    class ml{
+    
+    }
+    class ff{
+    
     }
 
+    //atribut
 public class halamanutama {
+    
+    //method
+    public void pubg(){
+        System.out.println("Pengisian UC PUBGM");
+        System.out.println("Daftar UC: ");
+        System.out.println("1. 60UC : 15rb");
+        System.out.println("2. 200UC : 50rb");
+        System.out.println("3. 620UC : 110rb");
+    }
+    public void ml(){
+        System.out.println("Pengisian Mobile Legends");
+        System.out.println("Daftar Diamond: ");
+        System.out.println("1. 60 Diamond : 15rb");
+        System.out.println("2. 286 Diamond : 70rb");
+        System.out.println("3. 756 Diamond : 130rb");
+    }
+    public void ff(){
+        System.out.println("Pengisian Diamond Free Fire");
+        System.out.println("Daftar Diamond: ");
+        System.out.println("1. 50 Diamond : 5rb");
+        System.out.println("2. 200 Diamond : 20rb");
+        System.out.println("3. 600 Diamond : 90rb");
+    }
+    public void pb(){
+        System.out.println("Pengisian Cash Point Blank");
+        System.out.println("Daftar Cash: ");
+        System.out.println("1. 1000 Cash : 10rb");
+        System.out.println("2. 2000 Cash : 19rb");
+        System.out.println("3. 5000 Cash : 49rb");
+    }
     public static void main(String[] args) {
+    String pembayaran;
+    String pubg;
+    String ml;
+    String ff;
+    String pb;
+    String total;
+    Double hitung;
+    byte pilih;
         Scanner input= new Scanner(System.in);
-        bayar data = new bayar();
-        byte pilih;
-        System.out.println("--------------------------------");
-        System.out.println("1. Beli UC PUBG ");
-        System.out.println("2. Beli Diamond Mobile Legend");
-        System.out.println("--------------------------------");
+        halamanutama menu = new halamanutama();
+        System.out.println("Pilih Menu : ");
+        System.out.println("1.PUBG, 2.ML ,3.FF, 4.PB");
         System.out.println("Masukan Pilihan : ");
         pilih = input.nextByte();
+        
         if (pilih == 1){
-            data.pubg();
+        menu.pubg();
         }else if (pilih == 2){
-           data.ml();
-    }
-        pembayaran pay = new pembayaran();
+        menu.ml();
+        }
+        else if (pilih == 3){
+            menu.ff();
+        }
+        else if (pilih == 4){
+            menu.pb();
+        }else{
+            System.out.println("Pilihan Tidak ada!!!");
+        }
+        
         
         
     }
