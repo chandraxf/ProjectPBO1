df70d6cadf9642107b98dcedc8f387dc35efe567
CHDHX <CHDHX@CHDHX-PC>
2021-06-20 22:05:54 +0700

    class3: Pembuatan Object ke class1

--- src/class3/class3.java
+++ src/class3/class3.java
@@ -3,6 +3,7 @@ import class1.class1;
 public class class3 {
     public static void main(String[] args) {
         System.out.println("Hi");
+        System.out.println("---");
         int a=1;
         class1 cls = new class1(a);
     }
